3b7f883a255639f71467a65a27e1eb037b8971d7
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2022-12-05 01:44:17 +0530

    Add files via upload

---
+++ Summary chapter-3.py
@@ -0,0 +1,34 @@
+# if statement 
+# name = "Ayush "
+# if name == "Ayush" or name == "ayush":
+#  print("you are ayush")
+# elif name == "mohit" or name == "Mohit":
+#  print(" you are mohit ")
+# else:    
+#  print(" you are not Ayush or mohit")
+
+# while
+# i = 0 
+# while i < 10:
+#  print("hello world")
+#   i += 1
+
+# for loop
+# for i in range(1,11,2):
+#    print(i)
+
+
+# break ketword
+for i in range(1,11):
+        if i == 5:
+            break
+        print(i)
+
+# continue keyword 
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)
+
+# for i in "harshit":
+#     print(i)
---
+++ for loop and string.py
@@ -0,0 +1,15 @@
+name = "harshit"
+# for i in range(len(name)):
+#    print(name[i])
+
+# name = "harshit"
+# for i in name:
+#  print(i)
+
+#1256 ----> 1+2+5+6
+num = input("enter a number : ")
+total = 0
+for i in num:
+    total += int(i)
+
+
