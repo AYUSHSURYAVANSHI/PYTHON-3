ceff893ad674b44f565c834f4b1b7c9746f16050
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-01-24 12:10:43 +0530

    Add files via upload

---
+++ Escape sequance.py
@@ -0,0 +1,92 @@
+print("hello world")
+
+print("my name is ram\\")
+print('hii\' this is my friend')
+print('what \' is your name ')
+print("hello'ayush'")
+print("hello\tbrother")
+
+print('hello\bo')
+print('line A\n line B')
+
+print('hello\\' )
+print('HELLO BROTHER\\n')
+print('what are you doing\\t')
+print('this is 2 backslash \\\\')
+
+# this is escape sequance 
+
+print('\\\" and \\\'')
+# \\=\
+#\'='
+#\" = "
+#\\\' = \'
+
+# raw string
+print(r'line A \n line B')
+print(r'line \t line B')
+
+# print emoji 
+
+print("\U0001F602")
+print("\U0001F603")
+print("\U0001F604")
+print("\U0001F610")
+
+# Calculator 
+
+print(3+2)
+print(3-4)
+print(4*5)
+print(5*4-4-6)
+print(2/4)
+print(4//4)
+print((2*5)-3)
+
+print(4%4)
+print(round(3**0.5))
+print(99-33)
+
+# Associativity Rule 
+
+print(8**4**3)
+# rule for variable's
+_name = "hello"
+print(_name)
+
+nam4e = 'hello'
+print(nam4e)
+
+# Naming convantions for Variable 
+
+User_One_Name = 'Rohit'
+print(User_One_Name )
+UserOneName = "Aakash"
+print(UserOneName)
+
+
+# string 
+
+my_name_is = "Harshit Sharma "
+print(my_name_is)
+
+copy_name = "log"
+print(copy_name)
+# string concatenation 
+First_name = "Ayush"
+Last_name = "Suryavanshi"
+print(First_name + Last_name)
+
+# print(First_name + 3) # can not do this 
+
+print(3*First_name)
+print(First_name + str(3))
+
+
+
+# input function 
+name = input(" Enter your name: ")
+print(name)
+
+age = input(int("Enter your age : "))
+print(age)
