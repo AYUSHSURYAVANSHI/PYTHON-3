e444bf17019136bd8b79a614419e380630116431
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-12 21:10:12 +0530

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,113 @@
+x = 5
+
+y = 3
+
+x = y
+
+y = x + 2
+
+print(x, y)
+
+a = 10
+
+b = 5
+
+c = a // b
+
+d = a % b
+
+print(c, d)
+
+
+x = 5
+
+y = 10
+
+x = y
+
+y = 20
+
+print(x)
+
+
+
+x = 5
+
+y = 10
+
+if x > y:
+
+    print("x is greater")
+
+else:
+
+    print("y is greater")
+
+
+count = 0
+
+while count < 5:
+
+    print(count)
+
+    count += 1
+
+sum = 0
+
+for i in range(1, 6):
+
+    sum += i
+
+print(sum)
+
+x = 10
+
+if x > 5:
+
+    print("x is greater than 5")
+
+elif x == 5:
+
+    print("x is equal to 5")
+
+else:
+
+    print("x is less than 5")
+
+
+my_list = [1, 2, 3]
+
+new_list = my_list
+
+new_list.append(4)
+
+print(my_list)
+
+
+x = 10
+
+if x > 5:
+
+    print("x is greater than 5")
+
+elif x == 5:
+
+    print("x is equal to 5")
+
+else:
+
+    print("x is less than 5")
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
